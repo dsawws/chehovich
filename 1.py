80c6e9405bedc449da052a6dafca8d8126846e3c
Mishanya <misha.gorelkin.gh@gmail.com>
2026-02-16 10:20:40 +0300

    sdfa

--- 1.py
+++ 1.py
@@ -1,2 +1,12 @@
-while True:
-    print(eval(input()))
+try :
+    a=int(input())
+    c=int(input())
+    b=a/c
+                                                                                                                                                                                                                                    
+
+except Exception:
+    print('ошибка')
+except ValueError:
+    print('другая ошибка')
+else:
+    print(b)    
--- Python/04.02.26/4.py
+++ Python/04.02.26/4.py
@@ -1,3 +1,4 @@
+import math
 def plus (a,b):
     print (a+b)
 def minus(a,b):
@@ -8,22 +9,47 @@ def f1(a,b):
     print(a/b)
 def z (a,b):
     print(a**b)
-
-
-
+def sin (a):
+    a_rad= math.radians(a)
+    print(math.sin(a_rad))
+def cos (a):
+    a_rad= math.radians(a)
+    print(math.cos(a_rad))
+def tan (a):
+    a_rad= math.radians(a)
+    print(math.tan(a_rad))
 
 while True:
-    a= int(input("число а "))
-    b=int(input("число b "))
-    c=input("оператор ")
+    try:
+        a= int(input("число а "))
+        b=int(input("число b "))
+        c=input("оператор ")
 
-    if c=="+":
-        plus(a,b)
-    elif c == "-":
-        minus(a,b)
-    elif c == "*":
-        f(a,b)
-    elif c == "/":
-        f1(a,b)
-    elif c=="**":
-        z(a,b)
+        if c=="+":
+            plus(a,b)
+        elif c == "-":
+            minus(a,b)
+        elif c == "*":
+            f(a,b)
+        elif c == "/":
+            f1(a,b)
+        elif c=="**":
+            z(a,b)
+        elif c =="sin":
+            sin(a)
+        elif c =="cos":
+            cos(a)
+        elif c =="tan":
+            tan(a)
+    except ValueError:
+        print('отшибка ввода')
+            
+    except ZeroDivisionError:
+        print('деление на ноль')
+
+
+
+
+
+
+
