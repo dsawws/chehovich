410fd03bccaf6a56475635d36d65e98abf267706
Mishanya <misha.gorelkin.gh@gmail.com>
2026-04-22 10:58:30 +0300

    asdas

---
+++ Python/22.04.26/5.py
@@ -0,0 +1,5 @@
+favorite_songs = {  
+'Серёга': ["Unforgiven", "Holiday", "Highway to hell"],  
+'Соня': ["Shake it out", "Don't stop me now", "Наше лето"],  
+'Дима': ["Владимирский централ", "Мурка", "Третье сентября"]
+}
